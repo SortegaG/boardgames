7a9dc63a492b44bf9d741e5e1602ce906a3aa1a7
Sergio Ortega <ortegagarrido.sergio@gmail.com>
2024-11-27 14:06:45 +0100

    fix/errors

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -7,7 +7,7 @@ router.get('/:email', userController.getUserByEmail);
 router.post('/', userController.createUser);
 router.put('/:email', userController.updateUser); // Sin terminar
 router.delete('/:email', userController.deleteUser);
-////
+//////
 
 
 
