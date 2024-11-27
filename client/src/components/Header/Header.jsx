8fbcabc38a5c82291c3842b04dfaa76245ca980a
Sergio Ortega <ortegagarrido.sergio@gmail.com>
2024-11-27 19:02:28 +0100

    heaverv1

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,9 +1,34 @@
-import React, { Component } from "react";
+// Contexto de usuario
+//import { UserContext } from "../../context/UserContext";
+import Nav from './Nav'
 
-class Header extends Component {
-  render() {
-    return <div>Header</div>;
-  }
-}
+const Header = () => {
+
+  //const navigate = useNavigate();
+  // // Consume el nombre del contexto
+  // //const { username, updateUsername } = useContext(UserContext);
+
+
+  // const handleOnClick = () => {
+  //   navigate('/');
+  // };
+  return (
+    // <div>
+    //   <p>Header</p>
+    // </div>
+  //   <Header>
+
+  //     {username?
+  //     <>
+  //       <span>Hola, {username}</span>
+  //       <button onClick={() => updateUsername("")}>Logout</button>
+  //     </>
+  //     :<button onClick={()=>handleOnClick()}>Login</button>}
+    <div>
+    <Nav />
+    <p>header jaja</p>
+    </div>
+  );
+};
 
 export default Header;
