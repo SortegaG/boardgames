92482ed9f979660ddd76db6e3f4909642cae89b7
Sergio Ortega <ortegagarrido.sergio@gmail.com>
2024-11-27 16:55:35 +0100

    fixed routes_games

--- queries/games.queries.js
+++ queries/games.queries.js
@@ -3,7 +3,7 @@ const queries = {
     getGameById: `
     SELECT *
     FROM games
-    WHERE id_juego = $1`,
+    WHERE LOWER(REPLACE(nombre, ' ', '')) = LOWER(REPLACE($1, ' ', ''))`,
     getAllGames: `SELECT * FROM games`,
     createGame: `INSERT INTO games (nombre, descripcion, categoria, edad_recomendada, jugadores_min, jugadores_max)
     VALUES ($1, $2, $3, $4, $5, $6);`,
@@ -11,6 +11,6 @@ const queries = {
     SET nombre = $1 , apellidos = $2, email = $3, password = $4
     WHERE email = $3`,
     deleteGame: `DELETE FROM games
-    WHERE id_juego = $1`, 
+    WHERE LOWER(id_juego) = LOWER($1)`, 
 }
 module.exports = queries;
