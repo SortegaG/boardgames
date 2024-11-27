b11b117b5be6f0925be2cf8a428a123be6502f95
Sergio Ortega <ortegagarrido.sergio@gmail.com>
2024-11-27 21:17:07 +0100

    headerV2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,23 @@
-import { useState } from 'react';
-import './App.css';
-import Header from './components/Header'
-import Footer from './components/Footer'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import './App.css'
+import Home from './components/Main/Home'
+// import Register from './components/Header/RegisterForm'
 import Main from './components/Main'
 
-function App() {
-  
-
+const App = () => {
   return (
-    <>
+    <Router>
       <Header />
-      <Main />
-      <Footer />
-    </>
-  )
-}
+      {/* <Main /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/register" element={<Register />} /> */}
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+
+export default App;
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,33 +1,50 @@
-// Contexto de usuario
-//import { UserContext } from "../../context/UserContext";
-import Nav from './Nav'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "../../styles/components/_Header.scss";
+import LoginForm from "./LoginForm";
+import RegisterForm from "./RegisterForm";
 
 const Header = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
+  const [showRegister, setShowRegister] = useState(false);
 
-  //const navigate = useNavigate();
-  // // Consume el nombre del contexto
-  // //const { username, updateUsername } = useContext(UserContext);
+  const handleLogin = () => setIsAuthenticated(true);
+  const handleLogout = () => setIsAuthenticated(false);
 
-
-  // const handleOnClick = () => {
-  //   navigate('/');
-  // };
   return (
-    // <div>
-    //   <p>Header</p>
-    // </div>
-  //   <Header>
+    <>
+      <header className="navbar">
+        <div className="nav-items">
+          <Link to="/" className="link">Inicio</Link>
+          {!isAuthenticated ? (
+            <>
+              <button onClick={() => setShowRegister(true)} className="button">
+                Registrarse
+              </button>
+              <button onClick={() => setShowLogin(true)} className="button">
+                Login
+              </button>
+            </>
+          ) : (
+            <>
+              <Link to="/profile" className="link">Perfil de Usuario</Link>
+              <button onClick={handleLogout} className="button">Logout</button>
+            </>
+          )}
+        </div>
+      </header>
 
-  //     {username?
-  //     <>
-  //       <span>Hola, {username}</span>
-  //       <button onClick={() => updateUsername("")}>Logout</button>
-  //     </>
-  //     :<button onClick={()=>handleOnClick()}>Login</button>}
-    <div>
-    <Nav />
-    <p>header jaja</p>
-    </div>
+      {/* Mostrar LoginForm si showLogin es true */}
+      {showLogin && (
+        <LoginForm onClose={() => setShowLogin(false)} onLogin={handleLogin} />
+      )}
+
+      {/* Mostrar RegisterForm si showRegister es true */}
+      {showRegister && (
+        <RegisterForm onClose={() => setShowRegister(false)} />
+      )}
+    </>
   );
 };
 
